c3511c29cf39434854c2844301218b230c2c95be
Gazzito <telmo9@hotmail.com>
2023-08-09 00:40:20 +0100

    Gallery done

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -55,9 +55,9 @@ const Gallery = () => {
             className="py-2 mb-4 text-xl font-montserrat font-light md:text-xl lg:text-xl"
           >
             My first app created with the frontend-skills above mentioned.
-            It shows a simple array of objects gathered from this website <span className="font-bold hover:text-secondary transition-colors"><a href="https://robohash.org/">https://robohash.org/</a></span>
+            It shows a simple array of objects gathered from this website <span className="font-bold  hover:text-secondary transition-colors"><a href="https://robohash.org/" target="_blank">https://robohash.org/</a></span>
           </Typography>
-          <div><Button>Find more</Button></div>
+          <div><Button className="bg-primary rounded-lg">Find more</Button></div>
           </div>
           
         </div>
