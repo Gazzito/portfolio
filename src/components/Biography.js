baf49f684c40983c4abd771f7c7ee596ff5bd3af
Gazzito <telmo9@hotmail.com>
2023-09-14 14:47:36 +0100

    Biography Fix

--- src/components/Biography.js
+++ src/components/Biography.js
@@ -23,7 +23,7 @@ const Biography = () => {
         <div className="hidden xl:flex items-center justify-center">
           <Typography
             variant="h3"
-            className="pl-6 mt-6 text-4xl font-montserrat text-white font-bold text-center leading-tight"
+            className="pl-6 mt-3 text-4xl font-montserrat text-white font-bold text-center leading-tight"
           >
             B <br></br>I <br></br>O<br></br> G<br></br> R<br></br> A <br></br>P{" "}
             <br></br>H <br></br>Y
@@ -38,21 +38,19 @@ const Biography = () => {
           </Typography>
         </div>
       </div>
-      <div className="bg-opacity-25 mt-3 rounded-lg py-0 px-4 md:px-6 md:col-span-3">
+      <div className="bg-opacity-25 mt-4 rounded-lg py-0 px-4 md:px-6 md:col-span-3">
         <Typography className="text-justify md:text-justify text-base font-montserrat font-extralight text-white leading-9">
           I started my journey by taking a Professional Course in the HighSchool
           of Entroncamento and proceeded furthermore to the PolyTechnic
           Institute of Tomar where I got my passion for programming, and by
-          using technical skills that i gathered over my previous academic
+          using technical skills that I gathered over my previous academic
           experiences I was able to accomplish Higher Technician Diploma in
           Technology and Programming in Information Systems (Level 5). At the
-          time being i'm finishing my Bachelor's Degree.<br></br>
+          time being I'm finishing my Bachelor's Degree.<br></br>
           My professional path began during my academic pursuit, when I started
-          an internship at "SSDMI" where i mantained the hardware of a
+          an internship at "SSDMI" where I mantained the hardware of a
           supermarket's network called "Os Mosqueteiros". I gained more software
-          knowledge by joining another academic internship in a company called
-          "Softinsa" where i was a web developer using Agile Methodology, and
-          later on joined them professionally for 9 months where i acquired more
+          knowledge by joining a company called "Softinsa" where I was a web developer using Agile Methodology, I acquired more
           soft skills and team building experiences. <b></b>
         </Typography>
       </div>
